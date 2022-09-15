b3a3e988055967d5d367f1267809a2bb133ff6d1
Kjeld Borch Egevang <kjeld@mail4us.dk>
2022-09-16 01:18:06 +0200

    Show latest prices

--- hour.php
+++ hour.php
@@ -13,11 +13,13 @@ class Fields extends Meter
 {
     public function Contents($body, $title = '')
     {
+        $script = basename($_SERVER['SCRIPT_NAME'], '.php');
+        $spot = substr($script, 0, 2) == 's_' ? 48 * 3600 : 0;
         $len = 30 * 3600;
         $start = $this->Get('start', date('Y-m-d', time() - $len));
         $time = strtotime($start);
-        $start = date('Y-m-d', $time);
-        $stop = date('Y-m-d', $time + $len);
+        $start = date('Y-m-d', $time + $spot);
+        $stop = date('Y-m-d', $time + $len + $spot);
         $this->Chart($body, $start, $stop, '', 11, 5);
     }
 }

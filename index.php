bd3ef0b4e960db7da15ec30f92c8c1aaaf91642a
Kjeld Borch Egevang <kjeld@mail4us.dk>
2022-10-04 09:02:03 +0200

    Use JavaScript for calculations

--- index.php
+++ index.php
@@ -7,54 +7,200 @@
  * file that was distributed with this source code.
  */
 
-include("page.php");
+ini_set('display_errors', 1);
+error_reporting(E_ALL | E_STRICT);
 
-class Index extends Page
+include("htmlnode.php");
+
+class Index
 {
-    public function Contents($body, $title = '')
+    public function DoCurl($url, $data = array(), $method = 'GET', $progress = null)
     {
-        $div = parent::Contents($body, 'Elforbrug');
-        $div->P('
-            Her kan du se hvad dit faktiske forbrug har været baseret
-            på timer, dage, måneder, kvartaler eller år.');
-        $div->P('
-            Dette gøres ved at udtrække spotpriser fra energidataservice.dk
-            og forbrug (pr. time) fra eloverblik.dk.');
-        $div->P('
-            De felter, der udfyldes under "indstillinger" gemmes
-            i cookies lokalt i din browser.');
-        $url = 'https://github.com/Gitdyr/elforbrug';
-        $p = $div->P("
-            Du kan downloade og installere koden fra GitHub:
-            <a href='$url'>$url</a>.");
-        $url = 'https://eloverblik.dk/';
-        $button = $div->Button('🞃');
-        $button->type('button');
-        $button->class('btn btn-secondary float-end');
-        $button->{'data-bs-toggle'}('collapse');
-        $button->{'data-bs-target'}('#instructions');
-        $div = $div->Div();
-        $div->class('collapse');
-        $div->id('instructions');
-        $div->P();
-        $p = $div->P("
-            For at kunne hente målerdata, skal du logge ind hos:
-            <a href='$url'>$url</a>.");
-        $div->P('Klik på ikonet i øverste højre
-            hjørne og vælg "Datadeling".');
-        $p = $div->P();
-        $img = $div->Img();
-        $img->src('eloverblik1.png');
-        $img->class('img-fluid mx-auto d-block rounded');
-        $div->P();
-        $div->P('
-            Følg vejledningen og opret et refresh token. Start
-            værktøjet og klik på "Indstillinger".<br>Her angives det
-            refresh token, du fik fra eloverblik.dk.');
+        $ch = curl_init();
+        if ($data && $method == 'GET') {
+            $filter = $data['filter'];
+            unset($data['filter']);
+            $query = http_build_query($data);
+            $query .= '&filter='.$filter;
+            $url .= '?'.$query;
+        }
+        if ($data && $method == 'POST') {
+            $json = json_encode($data, JSON_UNESCAPED_UNICODE);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+        }
+
+        if (isset($this->token)) {
+            $header[] = 'Content-type: application/json';
+            $header[] = 'Authorization: Bearer '.$this->token;
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+        }
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+        curl_setopt($ch, CURLOPT_FAILONERROR, true);
+        if ($progress) {
+            curl_setopt($ch, CURLOPT_WRITEFUNCTION, $progress);
+            // curl_setopt($ch, CURLOPT_PROGRESSFUNCTION, $progress);
+            curl_setopt($ch, CURLOPT_NOPROGRESS, false);
+            curl_setopt($ch, CURLOPT_BUFFERSIZE, 0x10000);
+        }
+
+        $data = curl_exec($ch);
+        if (curl_errno($ch)) {
+            $this->info = curl_getinfo($ch);
+            $error = curl_error($ch);
+            if ($error) {
+                $this->error = $error;
+            } else {
+                $this->error = "Connection failed";
+            }
+            curl_close($ch);
+            return false;
+        } else {
+            curl_close($ch);
+            return $data;
+        }
     }
+
+    public function HandlePost()
+    {
+        $json = file_get_contents('php://input');
+        if ($json) {
+            header('Cache-Control: no-store, no-cache, must-revalidate');
+            $data = json_decode($json);
+            switch ($data->action) {
+                case 'token':
+                    $this->token = $data->token;
+                    $url = 'https://api.eloverblik.dk/customerapi/api/token';
+                    print $this->DoCurl($url);
+                    break;
+                case 'points':
+                    $this->token = $data->token;
+                    $url = 'https://api.eloverblik.dk/customerapi/api';
+                    $url .= '/meteringpoints/meteringpoints';
+                    print $this->DoCurl($url);
+                    break;
+                case 'prices':
+                    $this->Prices($data->start, $data->stop, $data->area);
+                    break;
+                case 'quantity':
+                    $this->token = $data->token;
+                    $this->Quantity($data->start, $data->stop, $data->id);
+                    break;
+            }
+            if (!empty($this->error)) {
+                $msg = [
+                    'error' => $this->error,
+                    'info' => $this->info
+                ];
+                print json_encode($msg);
+            }
+            exit(0);
+        }
+    }
+
+    public function ProgressBar($resource, $str)
+    {
+        print $str;
+        flush();
+        ob_flush();
+        /*
+        $fp = fopen("kbe.txt", "a");
+        $this->file_len += strlen($str);
+        fprintf($fp, "%s", $str);
+        fclose($fp);
+        */
+        return strlen($str);
+    }
+
+    public function Prices($start, $stop, $price_area)
+    {
+        $prices = array();
+        $start = substr($start, 0, 16);
+        $start = strtotime($start);
+        $stop = substr($stop, 0, 16);
+        $stop = strtotime($stop);
+        $url = 'https://api.energidataservice.dk/dataset/Elspotprices';
+        $data = array(
+            'offset' => 0,
+            'sort' => 'HourUTC asc',
+            'timezone' => 'utc',
+            'start' => date('Y-m-d\TH:i', $start),
+            'end' => date('Y-m-d\TH:i', $stop),
+            'filter' => '{"PriceArea":"'.$price_area.'"}'
+        );
+        // $this->Dump($data);
+        $progress = array($this, 'ProgressBar');
+        $this->file_len = 0;
+        $res = $this->DoCurl($url, $data, 'GET', $progress);
+        /*
+        for ($j = 0; $j < 128; $j++) {
+            print "<!---------------------------->\n";
+        }
+        */
+        print "\n";
+        flush();
+        ob_flush();
+    }
+
+    public function Quantity($start, $stop, $id)
+    {
+        $start = strtotime($start);
+        $stop = strtotime($stop);
+        $sstart = date('c', $start);
+        $sstop = date('c', $stop);
+        if ($stop > time()) {
+            $stop = time();
+        }
+        $url = 'https://api.eloverblik.dk/customerapi/api';
+        $url .= '/meterdata/gettimeseries';
+        $url .= '/'.date('Y-m-d', $start);
+        $url .= '/'.date('Y-m-d', $stop);
+        $url .= '/Hour';
+        $data = json_decode('{"meteringPoints":{"meteringPoint":["'.$id.'"]}}');
+        $progress = array($this, 'ProgressBar');
+        $this->DoCurl($url, $data, 'POST', $progress);
+    }
+
+    public function Display()
+    {
+        $this->HandlePost();
+        $this->html = new HtmlNode();
+        $bpath = 'bootstrap';
+        $head = $this->html->Head();
+        $meta = $head->Meta();
+        $meta->name("viewport");
+        $meta->content("width=device-width, initial-scale=1");
+        $head->Title('Elforbrug');
+        $link = $head->Link();
+        $link->rel('stylesheet');
+        $link->href($bpath.'/css/bootstrap.min.css');
+        $link = $head->Link();
+        $link->rel('stylesheet');
+        $link->href('style.css');
+        $data = 'test';
+        $json = json_encode($data);
+        $script = $head->Script("
+            var php_result = {
+                'data':$json
+            }
+        ");
+        $head->Script()->src($bpath.'/js/bootstrap.min.js');
+        $head->Script()->src('js/htmlnode.js');
+        $head->Script()->src('js/page.js');
+        $head->Script()->src('js/chart.js');
+        $head->Script()->src('js/index.js');
+        $head->Script()->src('js/settings.js');
+        $head->Script()->src('js/points.js');
+        $head->Script()->src('js/meter.js');
+        $this->html->Body();
+        $this->html->Display();
+    }
+
 }
 
-$page = new Index();
-$page->Display();
+$index = new Index();
+$index->Display();
 
 ?>
